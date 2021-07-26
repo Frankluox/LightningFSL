0dda2176a5f9b7425cd5f6b42daa159684d959e0
“luoxu” <Frank.Luox@outlook.com>
2021-07-26 18:31:22 +0800

    load backbone

---
+++ utils.py
@@ -0,0 +1,13 @@
+
+def preserve_key(state, remove_prefix: str):
+    """Preserve part of model weights based on the
+       prefix of the preserved module name.
+    """
+    state_keys = list(state.keys())
+    for i, key in enumerate(state_keys):
+        if remove_prefix in key:
+            newkey = key.replace(remove_prefix, "")
+            state[newkey] = state.pop(key)
+        else:
+            state.pop(key)
+    return state
