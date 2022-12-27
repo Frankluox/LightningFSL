94427cfb426a72c0decbcba6c527bf924006fc7c
Xu Luo <Frank.Luox@outlook.com>
2022-12-27 20:38:50 +0800

    Update set_config_meta_baseline_pretrain.py

--- config/set_config_meta_baseline_pretrain.py
+++ config/set_config_meta_baseline_pretrain.py
@@ -103,7 +103,8 @@ def config():
     #less important
     per_gpu_val_batchsize = 8
     per_gpu_test_batchsize = 8
-    way = 5
+    val_way = 5
+    test_way = 5
     val_shot = 5
     num_query = 15
 
@@ -137,7 +138,8 @@ def config():
     data["test_shot"] = test_shot
     data["val_num_workers"] = 8
     data["is_DDP"] = True if multi_gpu else False
-    data["way"] = way
+    data["val_way"] = val_way
+    data["test_way"] = test_way
     data["val_shot"] = val_shot
     data["num_query"] = num_query
     data["drop_last"] = False
@@ -157,7 +159,8 @@ def config():
     #less important
     model["task_classifier_name"] = "proto_head"
     model["task_classifier_params"] = {"learn_scale":False}
-    model["way"] = way
+    model["val_way"] = val_way
+    model["test_way"] = test_way
     model["val_shot"] = val_shot
     model["test_shot"] = test_shot
     model["num_query"] = num_query
